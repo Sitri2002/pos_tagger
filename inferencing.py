613df13abbd417cc9eea2d391a60b53bcf50b0f5
Jack Nguyen <nguyenduyanh241002@gmail.com>
2023-09-15 15:10:51 -0700

    Update inferencing.py

--- inferencing.py
+++ inferencing.py
@@ -3,4 +3,6 @@ import parse_tag_prob as parse
 word_tag = parse.parse_word_tag("./train.txt")
 tag_tag = parse.parse_tag_tag("./train.txt")
 
-print(word_tag)
+def inference(input):
+    token = input.split(" ")
+    

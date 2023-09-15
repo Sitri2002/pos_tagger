d6e74f8c230a176a9c2b3be61f01e9abd0ce7a10
Jack Nguyen <nguyenduyanh241002@gmail.com>
2023-09-15 03:45:43 -0700

    Update parse_tag_prob.py

--- parse_tag_prob.py
+++ parse_tag_prob.py
@@ -2,14 +2,15 @@ def parse_word_tag(file):
     f = open(file, "r")
     word_tag_dict = {}
     for line in f.readlines():
+        if line == "\n":
+            line = "<s> <s> <s>"
         word_tag = line.split(" ")[:2]
-        if len(word_tag) == 2:
-            if word_tag[0] not in word_tag_dict.keys():
-                word_tag_dict[word_tag[0]] = {}
-            if word_tag[1] not in word_tag_dict[word_tag[0]].keys():
-                word_tag_dict[word_tag[0]][word_tag[1]] = 1
-            else:
-                word_tag_dict[word_tag[0]][word_tag[1]]+=1
+        if word_tag[0] not in word_tag_dict.keys():
+            word_tag_dict[word_tag[0]] = {}
+        if word_tag[1] not in word_tag_dict[word_tag[0]].keys():
+            word_tag_dict[word_tag[0]][word_tag[1]] = 1
+        else:
+            word_tag_dict[word_tag[0]][word_tag[1]]+=1
     for key1 in word_tag_dict.keys():
         count = sum(word_tag_dict[key1].values())
         for key2 in word_tag_dict[key1].keys():
@@ -21,10 +22,11 @@ def parse_tag_tag(file):
     tag_tag_dict = {}
     tag_list = []
     for line in f.readlines():
+        if line == "\n":
+            line = "<s> <s> <s>"
         line = line.split(" ")
-        if len(line) > 1:
-            tag = line[1]
-            tag_list.append(tag)
+        tag = line[1]
+        tag_list.append(tag)
     for i in range(len(tag_list)-1):
         t0 = tag_list[i]
         t1 = tag_list[i+1]
@@ -39,4 +41,4 @@ def parse_tag_tag(file):
         for key2 in tag_tag_dict[key1].keys():
             tag_tag_dict[key1][key2] /= count    
     return tag_tag_dict
-        
+
